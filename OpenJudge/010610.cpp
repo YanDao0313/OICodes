915c361c7bc319717e8726acb48c0d7e7e0eef07
YanDao0313 <3201176393@qq.com>
2023-10-04 03:35:48 +0000

    WA | update OpenJudge/010610.cpp.
    Signed-off-by: YanDao0313 <3201176393@qq.com>

--- OpenJudge/010610.cpp
+++ OpenJudge/010610.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 //long long a,b,sum;
 char a[202],b[202],c[202];
-int la,lb,lc,x,y,s; //lx字符长度，xys过程量 
-bool d=1; //默认有前导0 
+int la,lb,lc,x,y,s; //lx×Ö·û³¤¶È£¬xys¹ý³ÌÁ¿ 
+bool d=1; //Ä¬ÈÏÓÐÇ°µ¼0 
 
 int main() {
 	//cin>>a>>b;
 	//sum=(long long)a+b; //This is WRONG!!!
-	//cout<<sum; //会炸
+	//cout<<sum; //»áÕ¨
 	
 	//Reference
 	//https://www.cnblogs.com/caiyishuai/p/8325885.html
@@ -21,7 +21,7 @@ int main() {
 	la=strlen(a);
 	lb=strlen(b);
 	lc=max(la,lb);
-	if (la>=lb) { //让两个字符数组的位数相同，短的补0 
+	if (la>=lb) { //ÈÃÁ½¸ö×Ö·ûÊý×éµÄÎ»ÊýÏàÍ¬£¬¶ÌµÄ²¹0 
 		for (int i=0; i<=la-lb-1; i++) c[i]='0';
 		for (int i=la-lb; i<la; i++) c[i]=b[i-(la-lb)];
 
@@ -29,24 +29,24 @@ int main() {
 		for (int i=0; i<=lb-la-1; i++) c[i]='0';
 		for (int i=lb-la; i<lb; i++) c[i]=a[i-(lb-la)];
 		for (int i=0; i<lb; i++) a[i]=b[i];
-	} //注意，让a与c相加，b存放最终答案 
+	} //×¢Òâ£¬ÈÃaÓëcÏà¼Ó£¬b´æ·Å×îÖÕ´ð°¸ 
 	memset(b, 0, sizeof(0));
-	for (int i=lc-1; i>=0; i--) { //从个位开始加 
-		s=c[i]-'0'+a[i]-'0'+x;
-		x=0; //注意清零与进位 
+	for (int i=lc-1; i>=0; i--) { //´Ó¸öÎ»¿ªÊ¼¼Ó 
+		s=c[i]-'0'+a[i]-'0'+x; //²»¼Ó'0'»áWA 
+		x=0; //×¢ÒâÇåÁãÓë½øÎ» 
 		if (s>=10) {
-			x=s/10; //x>>要放进去的数，注意进位 
+			x=s/10; //x>>Òª½øÎ»µÄÊý£¬×¢Òâ 
 			b[i]=s%10+'0';
 		} else b[i]=s+'0';
 	}
-	if (x!=0) { //最后的x就成为了最高位的数 
+	if (x!=0) { //×îºóµÄx¾Í³ÉÎªÁË×î¸ßÎ»µÄÊý 
 		cout<<x;
 		for (int i=0; i<lc; i++) cout<<b[i];
 	} else {
 		for (int i=0; i<lc; i++) {
 			if (d==0) cout<<b[i];
-			if (d==1&&b[i]!='0') { //判断有无前导0 
-				f=0;
+			if (d==1&&b[i]!='0') { //ÅÐ¶ÏÓÐÎÞÇ°µ¼0 
+				d=0;
 				cout<<b[i];
 			}
 
