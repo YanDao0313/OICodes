83cd8fde220372089478b5c153d505d59c548e2d
Kris Yan <10350588+yandao0313@user.noreply.gitee.com>
2023-10-06 02:18:15 +0000

    Unknown | update MSOJ/1099递增序列.cpp.
    Signed-off-by: Kris Yan <10350588+yandao0313@user.noreply.gitee.com>

--- MSOJ/1099递增序列.cpp
+++ MSOJ/1099递增序列.cpp
@@ -1,20 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n,a[100010];
+int n,i,a[100010];
 
 int main() {
 	cin>>n;
-	for (int i=0; i<n; i++) cin>>a[i];
-	n+=1;
-	for (int i=0; i<n; i++) {
-		if (a[i]==a[i+1]) {
-			n-=1;
-			for (int j=i+1; j<n; j++) {
-				a[j]=a[j+1];
-			}
-		}
+	for (i=0; i<n; i++) cin>>a[i];
+	//n+=1;
+	//for (int i=0; i<n; i++) {
+	//	if (a[i]==a[i+1]) {
+	//		n-=1;
+	//		for (int j=i+1; j<n; j++) {
+	//			a[j]=a[j+1];
+	//		}
+	//	}
+	//}
+	for (i=0; i<n-1; i++) {
+		if (a[i]!=a[i+1]) cout<<a[i]<<' ';
 	}
-	for (int i=0; i<n; i++) cout<<a[i]<<' ';
+	cout<<a[i];
 	return 0;
 }
