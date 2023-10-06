15ee3d5c15d034c2cb3318221d4912c4f7436c16
Kris Yan <10350588+yandao0313@user.noreply.gitee.com>
2023-10-06 09:12:39 +0000

    未完成
    Signed-off-by: Kris Yan <10350588+yandao0313@user.noreply.gitee.com>

---
+++ OpenJudge/010904.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n,pj[110],py[110],gb[110],xb[110],lw[110];
+int money[110];
+
+int main() {
+	cin>>n;
+	for (int i=1; i<=n; i++)
+		cin>>?>>pj[i]>>py[i]>>gb[i]>>xb[i]>>lw[i];
+	for (int i=1; i<=n; i++) {
+		if (pj[i]>80&&lw[i]>=1) money[i]+=8000;
+		if (pj[i]>85&&py[i]>80) money[i]+=4000;
+		if (pj[i]//...)
+	}
+	return 0;
+}
