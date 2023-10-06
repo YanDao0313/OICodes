4bda817e9964222f81d40d4327d4458b9980a5a0
Kris Yan <10350588+yandao0313@user.noreply.gitee.com>
2023-10-06 02:17:49 +0000

    AC | update MSOJ/1090插入m个数.cpp.
    Signed-off-by: Kris Yan <10350588+yandao0313@user.noreply.gitee.com>

--- MSOJ/1090插入m个数.cpp
+++ MSOJ/1090插入m个数.cpp
@@ -10,22 +10,28 @@ int main() {
 	for (int i=0; i<m; i++) cin>>b[i];
 	for (int i=0; i<m; i++) {
 		x=b[i];
-		if (x<a[0]) {
-			for (int j=n; j>0; j--) a[j]=a[j-1];
-			a[0]=x;
-		} else if (x>a[n-1]) {
-			a[n]=x;
-		} else {
-			for (int j=0; j<n; j++) {
-				if (a[j]<x&&a[j+1]>x) {
-					for (int ii=n; ii>i+1; ii--) a[ii]=a[ii-1];
-					a[j+1]=x;
-					break;
-				}
-			}
+		//	if (x<a[0]) {
+		//		for (int j=n; j>0; j--) a[j]=a[j-1];
+		//		a[0]=x;
+		//	} else if (x>a[n-1]) {
+		//		a[n]=x;
+		//	} else {
+		//		for (int j=0; j<n; j++) {
+		//			if (a[j]<x&&a[j+1]>x) {
+		//				for (int ii=n; ii>i+1; ii--) a[ii]=a[ii-1];
+		//				a[j+1]=x;
+		//				break;
+		//			}
+		//		}
+		//	}
+		int j;
+		for (j=n-1; j>=0; j--) {
+			if (a[j]>x) a[j+1]=a[j];
+			else break;
 		}
+		a[j+1]=x;
+		n++;
 	}
-	n+=m;
-	for (int i=0; i<n; i++) cout<<a[i]<<' '; 
+	for (int i=0; i<n; i++) cout<<a[i]<<' ';
 	return 0;
 }
