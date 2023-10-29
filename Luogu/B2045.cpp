6b808e6baebe09d05fd8426caf2cf2b10d1fa287
YanDao0313 <10350588+yandao0313@user.noreply.gitee.com>
2023-10-29 09:38:04 +0000

    AC | upload Luogu/B2045.cpp.
    Signed-off-by: YanDao0313 <10350588+yandao0313@user.noreply.gitee.com>

---
+++ Luogu/B2045.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+int a;
+
+int main()
+{
+	cin>>a;
+	if(a!=1&&a!=3&&a!=5){
+		cout<<"YES";
+	}
+	else{
+		cout<<"NO";
+	}
+	return 0;
+}
