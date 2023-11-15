b198e803bd9206b74fdba90acbbd556e7a1159b2
YanDao0313 <10350588+yandao0313@user.noreply.gitee.com>
2023-11-15 12:43:56 +0000

    AC | upload Luogu/P5743.cpp.
    Signed-off-by: YanDao0313 <10350588+yandao0313@user.noreply.gitee.com>

---
+++ Luogu/P5743.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, ans = 1;
+
+int main() {
+	cin >> n;
+	for (int i = 1; i < n; i++) {
+		ans += 1;
+		ans *= 2;
+	}
+	cout << ans;
+	return 0;
+}
