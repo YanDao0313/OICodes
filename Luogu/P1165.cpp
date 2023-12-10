fd6c3a896f5417eeb5fa031507d5b0a7bc1e5ad5
yandao0313 <10350588+yandao0313@user.noreply.gitee.com>
2023-12-10 09:17:27 +0000

    AC | update Luogu/P1165.cpp.
    Signed-off-by: yandao0313 <10350588+yandao0313@user.noreply.gitee.com>

--- Luogu/P1165.cpp
+++ Luogu/P1165.cpp
@@ -1,26 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int s[200010], a[200010], top = 0;
-int n, q;
+int s[200010], top = 0;
+int n, q, t;
 
 int main() {
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		for (int j = 1; j <= top; j++) {
-			a[j] = s[j];
-		}
 		scanf("%d", &q);
 		if (q == 0) {
-			scanf("%d", &s[++top]);
+			scanf("%d", &t);
+			top++;
+			s[top] = max(s[top - 1], t);
 		} else if (q == 1) {
-			s[top] = 0;
-			top--;
+			if (top != 0)
+				top--;
 		} else if (q == 2) {
-			sort(a + 1, a + top + 1);
-			//cout << endl;
-			cout << a[top] << endl;
-			//cout << endl;
+			cout << s[top] << endl;
 		}
 	}
 	return 0;
