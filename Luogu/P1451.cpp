f97515286206ae686da7732cb3ab47928915cd25
yandao0313 <10350588+yandao0313@user.noreply.gitee.com>
2023-12-24 09:05:11 +0000

    AC | upload Luogu/P1451.cpp.
    Signed-off-by: yandao0313 <10350588+yandao0313@user.noreply.gitee.com>

---
+++ Luogu/P1451.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, h, t, ans;
+
+int mx[4] = {-1, 1, 0, 0}, my[4] = {0, 0, 1, -1};
+
+struct node {
+	int x, y;
+} q[10020];
+bool mapp[110][110], b[110][110];
+
+int main() {
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			scanf("%1d", &t);
+			if (t != 0)
+				mapp[i][j] = true;
+		}
+	}
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (!b[i][j] && mapp[i][j]) {
+				ans++;
+				h = 0, t = 1;
+				q[h].x = i, q[h].y = j;
+				while (h < t) {
+					int x = q[h].x, y = q[h].y;
+					for (int i = 0; i < 4; i++) {
+						int nx = x + mx[i], ny = y + my[i];
+						/*if (nx < 1 || nx > n || ny < 1 || ny > n)
+							continue;*/
+						if (mapp[nx][ny] && (!b[nx][ny])) {
+							q[t].x = nx, q[t].y = ny;
+							t++;
+							b[nx][ny] = 1;
+						}
+					}
+					h++;
+				}
+			}
+		}
+	}
+	cout << ans;
+	return 0;
+}
